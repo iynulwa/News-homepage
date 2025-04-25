e84699b1ca1daccbc3573c4dd0c36221cbc28ad5
iynulwa <joshuaiyanuoluwa1@gmail.com>
2025-04-25 02:57:55 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function showSideBar() {
+    const sideBar = document.querySelector('.side-bar');
+    sideBar.style.display = "flex"
+}
+
+function hideSideBar() {
+        const sideBar = document.querySelector('.side-bar');
+    sideBar.style.display = "none"
+}
